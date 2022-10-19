0be071ac93985915735c33d7df87b5d4ad201fd9
rehe3414 <rehe3414@uni.sydney.edu.au>
2022-10-20 00:47:44 +0800

    Adjust settings for MovieHub for importing problems

--- MovieHub/MovieHub/asgi.py
+++ MovieHub/MovieHub/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'movieHub.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MovieHub.settings')
 
 application = get_asgi_application()
--- MovieHub/MovieHub/wsgi.py
+++ MovieHub/MovieHub/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for movieHub project.
+WSGI config for MovieHub project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'movieHub.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MovieHub.settings')
 
 application = get_wsgi_application()
