300e3dda8df1e1e7f4784640d2d53a1937251a8b
Victor <xguo2443@uni.sydney.edu.au>
2022-10-21 14:35:23 +1100

    price

--- MovieHub/my_staff/views/manage.py
+++ MovieHub/my_staff/views/manage.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
+# Create your views here.
+from django.http import HttpResponse
 from my_admin.models import Release
 from my_admin.models import Movie
 from django.core.paginator import Paginator
 from datetime import datetime
 
-# Create your views here.
 
 # browse release information
-
 def index(request, pIndex=1):
     model = Release.objects
     releaseList = model.all()
@@ -30,7 +30,7 @@ def index(request, pIndex=1):
         else:
             is_delete = 'yes'
         releaseMovie = {'release_id': vo.release_id, 'movie_id': vo.movie_id, 'movie_name': movieName,
-                        'room_id': vo.room_id, 'release_time': vo.release_time, 'is_delete': is_delete}
+                        'room_id': vo.room_id, 'release_time': vo.release_time,'price':vo.price, 'is_delete': is_delete}
         releaseMovies.append(releaseMovie)
 
     # In page by 10 record
@@ -79,7 +79,10 @@ def update(request):
     try:
         releaseId = request.POST["releaseId"]
         ob = Release.objects.get(release_id=releaseId)
-        ob.release_time = request.POST['releaseTime']
+        newTime = request.POST['releaseTime']
+        if newTime:
+            ob.release_time = newTime
+        ob.price = request.POST['price']
         ob.update_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         ob.save()
         context = {'info': "Update Successfully"}
